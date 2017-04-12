5f4d5f35b9404f942cf42a2762cb30cf187ae926
tomisacat <wajmjjj@gmail.com>
2017-04-12 16:30:40 +0800

    get bitrate;

--- AudioTips/AudioFileOpenClose.playground/Contents.swift
+++ AudioTips/AudioFileOpenClose.playground/Contents.swift
@@ -3,11 +3,16 @@
 import UIKit
 import AudioToolbox
 
+// function
+
 func openFile() -> AudioFileID? {
     let url = Bundle.main.url(forResource: "guitar", withExtension: "m4a")
     var fd: AudioFileID? = nil
     AudioFileOpenURL(url! as CFURL, .readPermission, kAudioFileM4AType, &fd)
     
+    // Or in iOS platform, the file type could be 0 directly
+    // AudioFileOpenURL(url! as CFURL, .readPermission, 0, &fd)
+
     return fd
 }
 
@@ -15,8 +20,24 @@ func closeFile(fd: AudioFileID) {
     AudioFileClose(fd)
 }
 
-let fd = openFile()
+// use
+
+let fd: AudioFileID? = openFile()
 if let fd = fd {
     Swift.print("open file succeed")
+    
+    // get bitRate
+    var propertySize: UInt32 = 0
+    var writable: UInt32 = 0
+    var status = AudioFileGetPropertyInfo(fd, kAudioFilePropertyBitRate, &propertySize, &writable)
+    if status == noErr {
+        var bitRate: UInt32 = 0
+        AudioFileGetProperty(fd, kAudioFilePropertyBitRate, &propertySize, &bitRate)
+        print(bitRate)
+    }
+    
+    
+    
+    // finally close file
     closeFile(fd: fd)
 }
